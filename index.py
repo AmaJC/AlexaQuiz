bbcf582995c7769bd20b4e84c9387d703238a469
nwon4ever <nwon4ever@gmail.com>
2019-08-10 23:03:13 -0700

    Update index.py

--- index.py
+++ index.py
@@ -32,23 +32,30 @@ def on_launch(event):
     welcome_message += game_info["questions"][0][0]
     return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
 
-
+################################################################################################################################
 # TESTING choosing gamemode, rapid fire vs specific
 def on_launch(event):
     welcome_message = "Welcome to Where In The World Europe edition! Do you want rapid fire mode or mastery mode?"
     reprompt_message = "Try to get as many questions right as you can."
-    card_text = "Rapid faire or mastery mode?"
+    card_text = "Rapid fire or mastery mode?"
     card_title = "Welcome to WhereInTheWorld!"
 
-    if USER_RESPONSE==RapidFire:
+    if GAME_MODE==RapidFire:
         game_info = {"questions" : populate_game_questions("all"), "current_q_index" : 0, "score" : 0}
         welcome_message += game_info["questions"][0][0]
         return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
     else:
         welcome_message="Which country would you like to master? England, France, or Germany?"
-
-        return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
-
+        if USER_RESPONSE=="England":
+            game_info = {"questions" : populate_game_questions("England"), "current_q_index" : 0, "score" : 0}
+            return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
+        elif USER_RESPONSE=="France":
+            game_info = {"questions" : populate_game_questions("France"), "current_q_index" : 0, "score" : 0}
+            return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
+        elif USER_RESPONSE=="Germany":
+            game_info = {"questions" : populate_game_questions("Germany"), "current_q_index" : 0, "score" : 0}
+            return response_builder.build_json_response(welcome_message, card_text, card_title, reprompt_message, game_info, False)
+################################################################################################################################
 
 
 def on_session_end():
@@ -78,10 +85,10 @@ def handleAnswerRequest(event):
 	#if user_answer == all_game_questions[question_number][1]:
 		#event['sessionAttributes']["score"] += 1
 		#return response_builder.build_json_response(user_answer + " is correct!" + next_question, "","","", event['session']['attributes'], False)
-	#eturn response_builder.build_json_response(user_answer + " is incorrect!" + next_question, "","","", event['session']['attributes'], False)
+	#return response_builder.build_json_response(user_answer + " is incorrect!" + next_question, "","","", event['session']['attributes'], False)
 
 def handleDontKnowRequest(event):
-	pass
+    return response_builder.build_json_response("You suck. Next question", "","","", "", False)
 
 # SKILL SPECIFIC LOGIC
 
@@ -95,10 +102,22 @@ def populate_game_questions(mode):
 
 print(populate_game_questions()) 
 
+
+################################################################################################################################
 #TESTING populate game questions with modes
 def populate_game_questions(mode):
   """Build and return the list of questions for this game, no duplicates."""
-  indices = random.sample(range(0, len(quizquestion.questions_all)), 5) # If user doesn't specify, choose 5 random questions
-  return quizquestion.QuizQuestion.get_game_questions(indices)   
-
+  if mode=="all":
+    indices = random.sample(range(0, len(quizquestion.questions_all)), 5) # If user doesn't specify, choose 5 random questions
+    return quizquestion.QuizQuestion.get_game_questions(indices)
+  elif mode=="England":
+    indices = random.sample(range(0, len(quizquestion.questions_england)), 5) # If user doesn't specify, choose 5 random questions
+    return quizquestion.QuizQuestion.get_game_questions(indices)
+  elif mode=="France":
+    indices = random.sample(range(0, len(quizquestion.questions_france)), 5) # If user doesn't specify, choose 5 random questions
+    return quizquestion.QuizQuestion.get_game_questions(indices)  
+  elif mode=="Germany": 
+    indices = random.sample(range(0, len(quizquestion.questions_germany)), 5) # If user doesn't specify, choose 5 random questions
+    return quizquestion.QuizQuestion.get_game_questions(indices)
 print(populate_game_questions()) 
+################################################################################################################################
